14ac6b1c3cf81ce27142795b5d314c9ec0a89585
Chetan Choudhary <chetanchoudhary975@gmail.com>
2024-05-21 17:11:07 +0530

    Initial commit todo app

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Complete Python coding questions.",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Complete React learning.",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Complete Kubernetes learning.",
+        completed: false,
+    },
+];
